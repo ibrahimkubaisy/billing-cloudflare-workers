a1770781ffd97e0df6e34a74ccee19d103d0639b
Ibrahim Kubaisy <ibrahim.alkubaisy@gmail.com>
2024-10-26 22:55:34 +0300

    Update index.spec.ts

--- workers/subscriptions/test/index.spec.ts
+++ workers/subscriptions/test/index.spec.ts
@@ -1,25 +1,47 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
-import worker from '../src/worker';
+// import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+// import { describe, it, expect } from 'vitest';
+// import worker from '../src/worker';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+// // For now, you'll need to do something like this to get a correctly-typed
+// // `Request` to pass to `worker.fetch()`.
+// const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+// describe('Hello World worker', () => {
+// 	it('responds with Hello World! (unit style)', async () => {
+// 		const request = new IncomingRequest('http://example.com');
+// 		// Create an empty context to pass to `worker.fetch()`.
+// 		const ctx = createExecutionContext();
+// 		const response = await worker.fetch(request, env, ctx);
+// 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+// 		await waitOnExecutionContext(ctx);
+// 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+// 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+// 	it('responds with Hello World! (integration style)', async () => {
+// 		const response = await SELF.fetch('https://example.com');
+// 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+// 	});
+// });
+
+import { describe, it, expect, test } from 'vitest';
+import { Hono } from 'hono';
+import { testClient } from 'hono/testing';
+import app from '../src/worker';
+
+const MOCK_ENV = {
+	API_HOST: 'example.com',
+	DB: {
+		prepare: () => {
+			/* mocked D1 */
+		},
+	},
+};
+
+describe('Example', () => {
+	test('GET /customers', async () => {
+		const res = await app.request('/api/customers');
+		expect(res.status).toBe(200);
+		// expect(await res.text()).toBe('Many customers');
 	});
 });
